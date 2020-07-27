8e26f8872c4b3f31438b3468d8a88bf853330287
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-07-27 13:42:50 -0300

    utilizando styled-components

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-
-function Botao(props) {
-    return (
-        <a 
-            className={props.nomeDaClasse} 
-            href={props.enderecoDoLink}>
-            {props.nomeDoLink}
-        </a>
-    );
-}
+import styled from 'styled-components';
+/*
+    O styled component já possuí as props de acordo com a tag,
+    por exemplo no caso de uma tag <a> ele já pressupoe a existencia
+    de props como className e href.
+*/
+const Botao = styled.button`
+    color: var(--white);
+    border: 1px solid var(--white);
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 16px 24px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    outline: none;
+    border-radius: 5px;
+    text-decoration: none;
+    display: inline-block;
+    transition: opacity .3s;
+    &:hover,
+    &:focus {
+        opacity: .5;
+    }
+`;
 
 export default Botao; 
