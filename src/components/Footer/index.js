fbb66be7a6a3127ede2d98dba28d7d5e65c2b443
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-07-28 19:13:30 -0300

    componentes criados pela alura

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/logo.png';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Logo do CirinoFlix" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
