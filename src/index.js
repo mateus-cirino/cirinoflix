785b0a2c2ad5134c636a42a9030346720d9a95c8
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-07-28 21:36:31 -0300

    utilizacao do react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
