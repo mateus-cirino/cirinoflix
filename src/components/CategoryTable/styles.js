1099f063a58f7052e5da2eb8831c981c7cf285e4
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-07-30 21:17:15 -0300

    adicao de estilos novos para a tabela

--- src/components/CategoryTable/styles.js
+++ src/components/CategoryTable/styles.js
@@ -3,3 +3,37 @@ import styled from 'styled-components';
 export const TableContainer = styled.div`
     color: white;
 `;
+
+TableContainer.Table = styled.table`
+    width: 100%;
+    border: 1px solid #bbbbbc;
+    border-collapse: collapse;
+    text-align: center;
+`;
+
+TableContainer.Thead = styled.thead`
+    width: 100%;
+    color: #bbbbbc;
+`;
+
+TableContainer.Tbody = styled.tbody`
+    width: 100%;
+`;
+
+TableContainer.TdNome = styled.td`
+    width: 20%;
+    border: 1px solid #a9899e;
+    padding: 5px;
+`;
+
+TableContainer.TdDescricao = styled.td`
+    width: 60%;
+    border: 1px solid #a9899e;
+    padding: 5px;
+`;
+
+TableContainer.TdCor = styled.td`
+    width: 20%;
+    border: 1px solid #a9899e;
+    padding: 5px;
+`;
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Form = styled.form`
     background: #141414;
-    padding: 5px;
 `;
 
 export default Form;
